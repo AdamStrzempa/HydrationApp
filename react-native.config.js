dbc3f4903e8e10eacaeb53d80e500a7359ab900b
AdamStrzempa <adams@reactpoland.com>
2019-11-13 17:58:36 +0100

    init commit

---
+++ react-native.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  dependencies: {
+    'react-native-localize': {
+      platforms: {
+        ios: null
+      }
+    },
+    'react-native-gesture-handler': {
+      platforms: {
+        ios: null
+      }
+    },
+    'react-native-material-kit': {
+      platforms: {
+        ios: null,
+        android: null
+      }
+    }
+  }
+}
---
+++ src/screens/Home/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Home from './Home'
+import { useNavigation } from 'react-navigation-hooks'
+import { useDispatch, useSelector } from 'react-redux'
+import { compose } from 'ramda'
+import { login } from '../../redux/reducers/session/actions'
+
+const HomeScreen = () => {
+  const dispatch = useDispatch()
+  const reduxState = useSelector(state => state)
+  const { navigate } = useNavigation()
+  const props = {
+    login: compose(
+      dispatch,
+      login
+    ),
+    navigate,
+    reduxState
+  }
+  return <Home {...props} />
+}
+
+export default HomeScreen
